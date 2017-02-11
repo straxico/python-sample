9b4803b5cfcebe0b6a12377aad9e033b28762024
straxico <straxico@gmail.com>
2017-02-11 22:14:49 +0330

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,10 @@
 import os
 from flask import Flask
 import time
-import random
 import datetime
 import telepot
 from khayyam import JalaliDatetime
-import sys
 # sys.setdefaultencoding() does not exist, here!
-reload(sys)  # Reload does the trick!
-sys.setdefaultencoding('UTF8')
 
 app = Flask(__name__)
 
@@ -18,36 +14,45 @@ def hello():
     return 'strix bot'
 
 def handle(msg):
-    chat_id = msg['chat']['id']
+ content_type, chat_type, chat_id = telepot.glance(msg)
+ if content_type == 'text':
     command = msg['text']
     print ('Got command: %s' % command)
-    
-    if command == 'roll':
-        bot.sendMessage(chat_id, random.randint(1,6))
-    elif command == 'time':
-        bot.sendMessage(chat_id, str(datetime.datetime.now()))
-
-
     mss = command
-    
     mss=mss.replace('/', ',')
     mss=mss.replace('.', ',')
     mss=mss.replace('-', ',')
     mss=mss.split(',')
     now =JalaliDatetime.now()
-    old=JalaliDatetime(int(mss[0]),int(mss[1]),int(mss[2])).todate() 
-    dif = [100,200,222,300,333,400,444,500,555,600,666,700,777,800,888,900,999,1000,1111,2000,2222,3000,3333,4000,4444,5000,5555,6000,6666,7000,7777,8000,8228,8888,9999,10000,11111]
-    i=0
-    ss=''
-    for x in dif:
+    if len(mss)<3 :
+         if command == 'Time':
+             bot.sendMessage(chat_id, str(datetime.datetime.now()))
+         elif command == '/help':
+             sss= 'سلام واسه استفاده بايد تاريخ رو با يکي از فرمت هاي زير وارد کني' +'\n'+'1364/3/27'+'\n'+'1382-3-27'+'\n'+'1373.1.15'+'\n'+'1373,4,5'
+             bot.sendMessage(chat_id, str(sss))
+         elif command == '/start':
+             bot.sendMessage(chat_id, str("خیلی خوش اومدی لطفا یه تاریخ وارد کن"))
+         elif command == 'ممنون':
+             bot.sendMessage(chat_id, str("خواهش میکنم مهربون .. قابلی نداشت "))
+         elif command == 'احمق':
+             bot.sendMessage(chat_id, str("خودتی"))
+         else :
+           ss='اگه میشه تاریخ رو اینجوری وارد کن تا من بتونم بخونمش' + '\t' + '1372.11.5'
+    else:
+     old=JalaliDatetime(int(mss[0]),int(mss[1]),int(mss[2]))
+     dif = [100,200,222,300,333,400,444,500,555,600,666,700,777,800,888,900,999,1000,1111,2000,2222,3000,3333,4000,4444,5000,5555,6000,6666,7000,7777,8000,8228,8888,9000,9009,9999,10000,11111,12000,13000,14000,15000,16000,17000,18000,19000,20000,21000,22000,22222,23000,24000,25000,26000,27000]
+     i=0
+     d=(now-old).days
+     ss='سلام امروز ' +str(d)+ ' روزه هستی '
+     for x in dif:
         dif1=datetime.timedelta(x)
         date=old+dif1
         jdate=JalaliDatetime(date)
-        if (jdate>now and i<5):
-            ss= ss + '\n' + str(jdate.strftime('%A %D %B %N')) +'میشی '+str(dif1.days) +"روزه "
-            i=i+1
-
-    print(ss)   
+        if (jdate>now and i<10):
+             ss= ss + '\n' + str(jdate.strftime('%A %D %B %N')) +  ' میشی '+str(dif1.days) + " روزه "
+             i=i+1
+     ss= ss +'\n' +'\n' + 'مواظب خوبیات باش' +" @strixdaybot "
+    print(ss)
     bot.sendMessage(chat_id, text= ss )
 
 bot = telepot.Bot('375977039:AAEGag8W43sQmo61KmBnvtVXFOsVAP7PIwk')
