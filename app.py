141a74a2e13a5cd6876bb34c27823f0363024b92
mehran motiee <straxico@gmail.com>
2018-01-14 00:50:27 +0330

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,26 @@ import sys
 reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('UTF8')
 
+
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
 
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+    
 @app.route('/')
 def hello():
     return 'strix bot'
@@ -41,6 +56,9 @@ def handle(msg):
     mss=mss.replace('.', ',')
     mss=mss.replace('-', ',')
     mss=mss.split(',')
+    user = User(str(chat_id), msg)
+    db.session.add(user)
+    db.session.commit()
     now =JalaliDatetime.now()
     zz=""
     zzz=''
